bacdf615473d433a7beed9ead111f989390ef630
dev-buildit <developers.buildit@gmail.com>
2025-02-14 03:11:24 +0100

    Initial commit

--- form_email_sys/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FormData
-
-# Register your models here.
-@admin.register('FromData')
-class AdminFormData(admin.ModelAdmin):
-    list_display = ['name', 'email', 'message']
-    list_filter = ['-created-at']
--- form_email_sys/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormEmailSysConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'form_email_sys'
